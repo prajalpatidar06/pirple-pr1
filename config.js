ecf2ac8321b4f6b6bab2f09f42f28503de1139f5
Prajal Patidar <prajalpatidar01@gmail.com>
2022-06-27 12:29:58 +0530

    configuration

--- config.js
+++ config.js
@@ -3,15 +3,17 @@ var environment = {};
 //Staging (default) environment
 environment.staging = {
   httpPort: 3003,
-  httpsPort:3005,
+  httpsPort: 3005,
   envName: "staging",
+  hashingSecret: "thisIsASecret",
 };
 
 // Production object
 environment.production = {
   httpPort: 5000,
-  httpsPort:5001,
+  httpsPort: 5001,
   envName: "production",
+  hashingSecret: "thisIsAlsoASecret",
 };
 const currentEnvironment =
   typeof process.env.NODE_ENV === "string"
